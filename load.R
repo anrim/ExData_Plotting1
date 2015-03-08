f82935b02f8fb27527f5f55453c89204f9181ac5
anrim <a@rimbe.net>
2015-03-08 12:18:54 -0700

    Load house hold power data

--- load.R
+++ load.R
@@ -6,21 +6,19 @@ loadHCPData <- function () {
     unzip("data.zip")  
   }
   
-  # read data
+  # Read data
   df <- read.table("household_power_consumption.txt",
                      header=TRUE,
                      sep=";",
                      na.strings="?",
                      colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 
-  # select 2007-02-01 and 2007-02-02
+  # Select 2007-02-01 and 2007-02-02
   df <- df[(df$Date == "1/2/2007") | (df$Date == "2/2/2007"),]
   
-  # convert to date & time
-  df$Date <- as.Date(df$Date, format="%d/%m/%y")
-  df$Time <- strptime(df$Time, format="%H:%M:%S")
+  # Convert to date & time
+  df$DateTime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
   
-  #df$DateTime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
   df
 }
 
